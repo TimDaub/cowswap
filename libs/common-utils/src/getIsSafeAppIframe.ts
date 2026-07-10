9738bb8546de8e1de6a7dc87c102d4efd571dd7e
Tim Daubenschütz <timdaub@safe.global>
2026-07-10 13:40:10 +0200

    Add additional local URL to supported origins
    We tried to adjust our e2e tests to 4003, but this created even more problems and we are lazy and don't want to spend a lot of time adjusting everything when we can just add another URL here. Hope that is OK 🙏🏼 
    
    https://github.com/safe-global/safe-wallet-monorepo/pull/8283

--- libs/common-utils/src/getIsSafeAppIframe.ts
+++ libs/common-utils/src/getIsSafeAppIframe.ts
@@ -9,7 +9,8 @@ const SAFE_APP_PREVIEW_URLS = [
 ]
 const SAFE_APP_PREVIEW_HOST_SUFFIX = '.review.5afe.dev' // Safe's per-PR review env
 const SAFE_APP_LOCAL_URL = 'http://localhost:4003'
-const SAFE_SUPPORTED_ORIGINS = [SAFE_APP_ORIGIN, ...SAFE_APP_PREVIEW_URLS, SAFE_APP_LOCAL_URL].map(
+const SAFE_APP_LOCAL_URL2 = 'http://localhost:8080'
+const SAFE_SUPPORTED_ORIGINS = [SAFE_APP_ORIGIN, ...SAFE_APP_PREVIEW_URLS, SAFE_APP_LOCAL_URL1, SAFE_APP_LOCAL_URL2].map(
   (origin) => new URL(origin).origin,
 )
 
